f116016f74bb1bb9c98a213e199932b091c14827
Junior <hrjr931@gmail.com>
2025-05-08 12:09:53 +0800

    Lab2_v1.0

---
+++ bmi.py
@@ -0,0 +1,17 @@
+def calculate_bmi(height, weight):
+    print("Height = " + str(height))
+    print("Weight = " + str(weight))
+    
+    bmi = weight / (height ** 2)
+    
+    if bmi < 18.5:
+        print("Your BMI is: ", bmi)
+        print("you are under weight.")
+    elif bmi <= 25.0:
+        print("Your BMI is: ", bmi)
+        print("you are normal weight.")
+    else:
+        print("Your BMI is: ", bmi)
+        print("you are over weight.")
+
+calculate_bmi(weight=57, height=1.73)
